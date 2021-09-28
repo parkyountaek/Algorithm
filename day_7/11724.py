17b75bd13d67a6bb3cda5d0a6974695a208e66c7
parkyountaek <yountaek11@gmail.com>
2021-09-28 22:12:17 +0900

    day_7

--- day_7/11724.py
+++ day_7/11724.py
@@ -4,7 +4,7 @@ sys.setrecursionlimit(10000)
 def solution(V):
     visited[V]=True
     for index in range(1, N+1):
-        if not visited[index] == 0 and matrix[V][index] == 1:
+        if not visited[index] and matrix[V][index] == 1:
             solution(index)
 
 N, M = map(int, input().split())
@@ -21,3 +21,25 @@ for i in range(1,N+1):
         solution(i)
         num+=1
 print(num)
+
+
+# import sys
+# sys.setrecursionlimit(10000)
+# n, m = map(int, sys.stdin.readline().split())
+# s = [[0] * (n + 1) for i in range(n + 1)]
+# visit = [0 for i in range(n + 1)]
+# cnt = 0
+# def dfs(i):
+#     visit[i] = 1
+#     for k in range(1, n + 1):
+#         if s[i][k] == 1 and visit[k] == 0:
+#             dfs(k)
+# for i in range(m):
+#     a, b = map(int, sys.stdin.readline().split())
+#     s[a][b] = 1
+#     s[b][a] = 1
+# for i in range(1, n + 1):
+#     if visit[i] == 0:
+#         dfs(i)
+#         cnt += 1
+# print(cnt)
