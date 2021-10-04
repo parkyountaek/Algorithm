632e96318829ddc7660f80ed61f23327452a1bd0
parkyountaek <yountaek11@gmail.com>
2021-10-04 23:39:36 +0900

    day_11

---
+++ day_11/2839.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+cnt = 0
+while True:
+    if N % 5 == 0:
+        cnt+=N//5
+        print(cnt)
+        break
+    N-=3
+    cnt+=1
+    if N < 0:
+        print("-1")
+        break
