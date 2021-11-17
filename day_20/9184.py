8b5b42328129f9cdee8f1778582d8452165f1627
박윤택 <parkyountaek@users.noreply.github.com>
2021-11-17 15:27:22 +0900

    day_20 dp

---
+++ day_20/9184.py
@@ -0,0 +1,30 @@
+import sys
+
+#i think memoization needs!!
+def w(a, b, c):
+  if a <= 0 or b <= 0 or c <= 0:
+    return 1
+  if a > 20 or b > 20 or c > 20:
+    return w(20, 20, 20)
+
+  if matrix[a][b][c]:
+    return matrix[a][b][c]
+  
+  if a < b and b < c:
+    matrix[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+    return matrix[a][b][c]
+  
+  matrix[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+  return matrix[a][b][c]
+
+matrix=[[[0]*50 for _ in range(50)] for __ in range(50)]
+
+while True:
+  a, b, c = map(int, sys.stdin.readline().split())
+  if a == b == c == -1:
+    break
+
+  result = w(a, b, c)
+  print("w({a}, {b}, {c}) = {result}".format(a=a, b=b, c=c, result=result))
+
+
