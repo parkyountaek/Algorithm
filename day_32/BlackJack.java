5f5fffa22991cdac30eb79aececed55aa9b8d803
unknown <yountaek11@gmail.com>
2022-05-07 17:45:52 +0900

    2798 bruteforce

--- day_32/BlackJack.java
+++ day_32/BlackJack.java
@@ -25,7 +25,6 @@ public class BlackJack {
         for (int k = j + 1; k < N; k++) {
           if (card[i] + card[j] + card[k] <= M && card[i] + card[j] + card[k] > total) {
             total = card[i] + card[j] + card[k];
-            System.out.println(String.format("%d %d %d = %d", card[i], card[j], card[k], total));
           }
         }
       }
