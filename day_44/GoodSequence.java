888065f14a0f07590058ea6cc670e36ff19c2e64
unknown <yountaek11@gmail.com>
2022-06-21 21:20:27 +0900

    baekjoon 2661

--- day_44/GoodSequence.java
+++ day_44/GoodSequence.java
@@ -24,7 +24,6 @@ public class GoodSequence {
       if (isGood(answer + candidate.toString()))
         make(answer + candidate.toString());
     }
-
   }
 
   public static boolean isGood(String sequence) {
