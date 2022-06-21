f799d444d781a18d6a3e172cc78b7dd8b0d8a394
unknown <yountaek11@gmail.com>
2022-06-21 20:43:19 +0900

    lib optimize

--- day_42/NQueens.java
+++ day_42/NQueens.java
@@ -3,7 +3,6 @@ package day_42;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /* baekjoon 9663 */
